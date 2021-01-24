aaf9b7088be93cc5804e99b37d181701be17a16a
Kaj Białas <kaj.bialas@dev-industry.com>
2021-01-24 14:48:37 +0100

    issue6

--- application/fileSystem.js
+++ application/fileSystem.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
+const _ = require('lodash');
 
-// fs.readFile('./text.txt', 'utf8', (err, data) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log(data);
-// });
-//
-// try {
-//   const data = fs.readFileSync('./text.txt', 'utf8');
-//   console.log(data);
-// } catch (err) {
-//   console.log(err);
-// }
+
+const users = [];
+
+try {
+  const data = fs.readFileSync('./users.json', 'utf8');
+  JSON.parse(data).map(user => users.push({id: user.id, name: user.name}));
+} catch (err) {
+  console.log(err);
+}
+
+const sortedArray = _.sortBy(users, ['name']);
+
+fs.writeFile('sortedUsers.json', JSON.stringify(sortedArray), (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  return;
+});
