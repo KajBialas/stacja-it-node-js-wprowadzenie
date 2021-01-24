540c1b741802275a4ff89ed5e1105956d32e89ec
Kaj Białas <kaj.bialas@dev-industry.com>
2021-01-24 16:02:08 +0100

    issue8

--- application/index.js
+++ application/index.js
@@ -1,18 +1,30 @@
 const http = require('http');
 const colors = require('colors');
+const fs = require('fs');
+
 import { responseHomeBody } from './routes/home';
 import { responseAboutBody } from './routes/about';
 import { response404Body } from './routes/page404';
 const PORT = 8000;
+
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {
     'Content-Type': 'text/html'
   });
 
+  console.log(request.url)
   switch (request.url) {
     case '/': response.end(responseHomeBody); break;
     case '/about': response.end(responseAboutBody); break;
+    case '/contact':
+      fs.readFile('./static/contact.html', (err, page) => {
+        if (err) {
+          console.log(err);
+        }
+        response.end(page);
+      });
+      break;
     default: response.end(response404Body);
   }
 });
