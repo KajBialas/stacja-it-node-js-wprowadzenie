4a2d49f996ffd97808a0595a7a0ad69d87935b27
Kaj Białas <kaj.bialas@dev-industry.com>
2021-01-24 12:10:06 +0100

    issue 5 init

---
+++ application/fileSystem.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// fs.readFile('./text.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+try {
+  const data = fs.readFileSync('./text.txt', 'utf8');
+  console.log(data);
+} catch (err) {
+  console.log(err);
+}
